ce6ffe1388af1ec29e20714cad2e0ae036175abc
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-10 00:14:15 +0200

    0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """Basic flask app """
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def homePage():
-    return render_template('templates/0-index.html') 
+    """home page"""
+    return render_template('0-index.html')
