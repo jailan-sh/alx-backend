0ab936295c1021df3e7f95ecd9ee7ba84e7b9f75
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-10 01:33:15 +0200

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,13 +4,24 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
+class Config():
+    """
+    configure the avaliable langs in the app
+    """
+    LANGUAGE = ['en', 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
-@app.route('/')
-def homePage():
+@app.route('/', strict_slash=False)
+def homePage() -> str:
     """home page"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == ('__main__'):
