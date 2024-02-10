e483931b92a77691865aad6d7c2b8f41bdfba19e
jailan-sh <jilan.elsherbiny@gmail.com>
2024-02-10 02:47:20 +0200

    up

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Basic flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    configure the avaliable langs in the app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def homePage() -> str:
+    """home page"""
+    return render_template('3-index.html')
+
+
+if __name__ == ('__main__'):
+    app.run()
